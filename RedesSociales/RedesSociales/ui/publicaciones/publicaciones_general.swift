37734514d244f2bd2897669f265054535a0fcd2b
alumno <alumno@pc-rolmos.uacj.mx>
2025-03-26 10:04:52 -0600

    [ADD] ejercicio

---
+++ RedesSociales/RedesSociales/ui/publicaciones/publicaciones_general.swift
@@ -0,0 +1,35 @@
+//
+//  publicaciones_general.swift
+//  RedesSociales
+//
+//  Created by alumno on 3/24/25.
+//
+
+import SwiftUI
+ 
+struct GeneralPublicaciones: View {
+   // @StateObject var controlador = ControladorPublicaciones() //guardamos la informacion quequeremos guardar
+    @Environment(ControladorAplicacion.self) var controlador
+    
+    var body: some View {
+        ScrollView{
+            VStack{
+                ForEach(controlador.publicaciones){ publicacion in
+                    Text("\(publicacion.id)")
+                    VStack{
+                        Text("\(publicacion.title)")
+                        Text("\(publicacion.body)")
+                    }
+                }
+            }
+        }
+        .onAppear{
+            print("Hi world")
+        }
+    }
+}
+
+#Preview {
+    GeneralPublicaciones()
+        .environment(ControladorAplicacion())
+}
